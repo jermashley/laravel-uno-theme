0446eb5f82659be841fabf594ea0a633be894c26
Jeremiah Ashley <jeremiah@prologuetechnology.com>
2017-09-29 15:56:53 -0500

    Added homepage boilerplate

---
+++ src/views/home/index.blade.php
@@ -0,0 +1,36 @@
+@extends('uno-theme::base')
+
+@section('hero')
+
+    <div id="hero">
+
+        <div class="hero content">
+
+            <div class="logo">
+
+                <img src="{{ asset('images/full-logo-light.png')}}" class="img-fluid" alt="">
+
+            </div>
+
+            <div class="snippet cta">
+
+                <h2>Edit Me</h2>
+
+                <div>
+                    <a href="#" class="btn btn-primary mx-3">Learn More</a>
+                    <a href="#" class="btn btn-primary mx-3">Contact Us</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
+
+
+
+@section('content')
+
+@endsection
