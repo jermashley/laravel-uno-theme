215e536a47b22df79a94730197bb5bd027b90085
Eric Percifield <epercifield@prologuetechnology.com>
2017-09-29 12:07:43 -0500

    Fix paths.

--- src/UnoServiceProvider.php
+++ src/UnoServiceProvider.php
@@ -6,29 +6,28 @@ use Illuminate\Support\ServiceProvider;
 
 class UnoServiceProvider extends ServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+	/**
+	 * Perform post-registration booting of services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$sass = realpath(__DIR__ . '/assets/sass');
+		$js = realpath(__DIR__ . '/assets/js');
+		$routes = realpath(__DIR__ . '/routes.php');
+		$views = realpath(__DIR__ . '/views/app');
 
-        $sass = __DIR__ . 'assets/sass';
-        $js = __DIR__ . 'assets/js';
+		$this->loadRoutesFrom($routes);
 
-        $this->loadRoutesFrom(__DIR__ . 'routes.php');
+		$this->publishes([
+			$sass => resource_path('assets/sass'),
+		], 'sass');
 
-        $this->publishes([
-            $sass => resource_path('assets/sass')
-        ], 'sass');
+		$this->publishes([
+			$js => resources_path('assets/js'),
+		], 'js');
 
-        $this->publishes([
-            $js => resources_path('assets/js')
-        ], 'js');
-
-        $this->loadViewsFrom(__DIR__ . 'views/app', 'uno-theme');
-
-    }
-
+		$this->loadViewsFrom($views, 'uno-theme');
+	}
 }
