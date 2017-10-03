f59e823cfa6ed5f1baa64396b548aa59a3285e7d
Jeremiah Ashley <jeremiah@prologuetechnology.com>
2017-10-03 15:31:33 -0500

    Added tag Card and styles for such

---
+++ src/views/components/tagCard.blade.php
@@ -0,0 +1,11 @@
+<div class="tagCard animated small">
+
+    <a href="{{ $tagCardLink }}" class="baseCard__link">
+
+        <div class="baseCard__heading small">
+            <span>{{ $tagCardIcon }} {{ $tagCardHeading }}</span>
+        </div>
+
+    </a>
+
+</div>
