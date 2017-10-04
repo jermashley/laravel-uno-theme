78c810b3243bb81ae93ea8f7e38817aa55233aea
Jeremiah Ashley <jeremiah@prologuetechnology.com>
2017-10-04 13:06:55 -0500

    Removed Blog view (haven't figured out nested extends)

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,3 @@
 Route::get('app.base', function(){
     return view('uno-theme::app.base');
 })->name('base');
-
-Route::get('app.blog', function(){
-    return view('uno-theme::app.blog');
-})->name('blog');
