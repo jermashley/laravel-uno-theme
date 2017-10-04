4b8fb1c367dcc072cc0f9b23bc277e45ae6ad800
Jeremiah Ashley <jeremiah@prologuetechnology.com>
2017-10-04 10:33:19 -0500

    Added assets for blog theme

--- src/routes.php
+++ src/routes.php
@@ -3,3 +3,7 @@
 Route::get('app.base', function(){
     return view('uno-theme::app.base');
 })->name('base');
+
+Route::get('app.blog', function(){
+    return view('uno-theme::app.blog');
+})->name('blog');
