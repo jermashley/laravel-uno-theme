d4166fc245e760d330a4c82409ee7bc2f5222d1f
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-10-09 14:37:24 -0500

    Added summaryCard back in.  Don't remember why it left.

---
+++ src/views/components/summaryCard.blade.php
@@ -0,0 +1,23 @@
+<div class="baseCard animated small">
+
+    <a href="{{ $summaryCardLink }}" class="baseCard__link">
+
+        <div class="baseCard__heading small">
+            <span>{{ $summaryCardHeading }}</span>
+        </div>
+
+        <div class="baseCard__divider"></div>
+
+        <div class="baseCard__text">
+            <div class="smallTruncated">
+                {{ $summaryCardText }}
+            </div>
+        </div>
+
+        <div class="baseCard__more">
+            <span>Read More</span>
+        </div>
+
+    </a>
+
+</div>
