f91c9ae43ad86d861ebb3aaa3c0ae684530a5a59
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-12-11 16:53:21 -0600

    Nav items with sub-links unfold with click, parent only directs on mobile.

--- src/assets/js/laravel-uno-theme/_navigation.js
+++ src/assets/js/laravel-uno-theme/_navigation.js
@@ -6,13 +6,50 @@ $('.nav-button').click(function(){
 
 
 if($(window).width() > 1024) {
-    $('.parentMenu').hover(
-        function(){
-            console.log('you done hovered');
-            $(this).find('#navigation').fadeIn(150);
-        },
-        function(){
-            $(this).find('#navigation').fadeOut(150);
+    $('.parentMenu').click(function(){
+
+            var clickedLink = $(this);
+
+            $(clickedLink).find('#navigation').click(function(e) {
+                
+                e.stopPropagation();
+                
+            });
+
+            console.log('You clicked to open the submenu.');
+
+            $(clickedLink).find('#navigation').fadeToggle(150);
+
+            $(document).keyup(function(e){
+                
+                if(e.keyCode === 27)
+    
+                    $(clickedLink).find('#navigation').fadeOut(250);
+    
+            });
+
+            $(document).click(function(e) {
+                
+                if(!$(e.target).closest(clickedLink).length) {
+        
+                    $(clickedLink).find('#navigation').fadeOut(250);
+        
+                }
+                
+            });
         }
     );
+} else {
+    $('.parentMenu').click(function() {
+
+        var clickedLink = $(this);
+        var dataLink = clickedLink.find('.parentLink').data('link');
+
+        $(document).ready(function() {
+            console.log(dataLink);
+        });
+
+        location.href = dataLink;
+
+    });
 }
