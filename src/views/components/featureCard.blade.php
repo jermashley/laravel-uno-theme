3489a8a5abb27d0936653ceaf778af49d583145f
Jeremiah Ashley <jeremiah.ashley@live.com>
2017-12-22 14:37:29 -0600

    Added target to featureCard Link

--- src/views/components/featureCard.blade.php
+++ src/views/components/featureCard.blade.php
@@ -1,6 +1,6 @@
 <div class="baseCard animated small">
 
-    <a href="{{ $featureCardLink }}" class="baseCard__link">
+    <a href="{{ $featureCardLink }}" target="{{ $featureCardTarget or '_self' }}" class="baseCard__link">
 
         <div class="baseCard__heading small">
             <span>{{ $featureCardHeading }}</span>
